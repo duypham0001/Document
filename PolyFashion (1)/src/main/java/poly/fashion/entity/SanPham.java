a8cc37f43f54b0d145ea5c6072e3ca66db4da253
duypham0001 <duypvkts02006@gmail.com>
2025-07-18 13:17:52 +0700

    Add files via upload

---
+++ PolyFashion (1)/src/main/java/poly/fashion/entity/SanPham.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+
+public class SanPham {
+    private String maSanPham;
+    private String tenSanPham;
+    private String moTa;
+    private double gia;
+    private int soLuongTonKho;
+
+    // Constructor, getters, setters
+}
